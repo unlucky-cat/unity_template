127685deb6e93b023f400bc196ba8ca42d712a32
Andrey Morozov <andrey@morozov.lv>
2019-02-11 23:26:26 +0200

    added LevelManager, prefabs and tileset

---
+++ Unity/UnityProject/Assets/Code/Components/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Code.Components
+{
+    public class LevelManager : MonoBehaviour
+    {
+        private int levelWidth;
+        private int levelHeight;
+
+        [SerializeField]
+        private Transform grassTile;
+        [SerializeField]
+        public Transform wallsTile;
+
+        [SerializeField]
+        private Color grassColor;
+        [SerializeField]
+        private Color wallsColor;
+
+        [SerializeField]
+        private Texture2D levelTexture;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
